e4403effb2b40d5edb6419480bdedc09fc2bed39
faisalfani <177006047@student.unsil.ac.id>
2020-04-30 08:30:50 +0100

    adding frontEnd

--- app.js
+++ app.js
@@ -13,8 +13,11 @@ setInterval(dataSource.updateDataCountries, 1000 * 60 * 60 * 24);
 
 const app = express();
 app.use(cors());
-
+app.use(express.static("public"));
 // use the api route
 app.use("/api", api);
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
 
 app.listen(port, () => console.log("server is start on 3000"));
